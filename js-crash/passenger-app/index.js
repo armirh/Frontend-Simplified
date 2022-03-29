5b426330f027daa8b312642a4554286cbf13f506
Armir Halimi <armirhalimi15@gmail.com>
2022-03-29 18:16:20 -0500

    Javascript Challenges

---
+++ js-crash/passenger-app/index.js
@@ -0,0 +1,19 @@
+//document.getElementById("count-el").innerText = 5
+
+// initialize the count to 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el")
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.innerHTML = count;
+}
+
+function decrement() {
+    count -= 1;
+    countEl.innerHTML = count
+}
